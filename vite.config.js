e0db0d3eee4d5eeb0f967c0e4ae50741e1bed4ff
Soutert <hnalvarez@unah.hn>
2025-06-26 00:29:37 -0600

    actualiza dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import purgeCss from 'vite-plugin-purgecss'
 
 export default defineConfig({
-  base: '/viteportafolio/',
+  base: '/VitePortafolio/',
   plugins: [
     purgeCss({
       content: ['./index.html', './script.js'],
